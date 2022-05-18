70ec31b005053866f1857a585f0c72d98d253e8f
Briojas <blake.a.riojas@gmail.com>
2022-05-18 12:47:20 -0500

    update queue functions, added mqtt-relay calls

--- brownie-chainlink/contracts/DaDerpyDerby.sol
+++ brownie-chainlink/contracts/DaDerpyDerby.sol
@@ -7,10 +7,9 @@ import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
     //using the "Iterable Mappings" Solidity example
 struct submission {
-        uint keyIndex; //storage position in the queue array
-        uint queueIndex; //position in the queue for execution
-        bytes32 script_cid_1; //first 32 bytes of the script CID uploaded to IPFS
-        bytes32 script_cid_2; //remaining 27 bytes
+        uint key_index; //storage position in the queue array
+        uint queue_index; //position in the queue for execution
+        bytes32[2] script_cid; //script IPFS CID broken into halves
         bool executed; //sent for execution
         uint score; //score after execution finished
     }
@@ -31,35 +30,35 @@ type Iterator is uint;
 
 library queueManagement {
     function insert(submission storage self, uint key, bytes32 script_cid_1, bytes32 script_cid_2) internal returns (bool replaced) {
-        uint keyIndex = self.data[key].keyIndex;
+        uint key_index = self.data[key].key_index;
         self.queue_ticket_no = self.queue_ticket_no + 1;
-        self.data[key].queueIndex = self.queue_ticket_no;
-        self.data[key].script_cid_1 = script_cid_1;
-        self.data[key].script_cid_2 = script_cid_2;
+        self.data[key].queue_index = self.queue_ticket_no;
+        self.data[key].script_cid[0] = script_cid_1;
+        self.data[key].script_cid[1] = script_cid_2;
         self.data[key].executed = false;
-        if (keyIndex > 0){ //checks if key already existed, and was overwritten
-            self.keys[keyIndex].deleted = false; //a deleted submission overwritten should not be marked deleted
+        if (key_index > 0){ //checks if key already existed, and was overwritten
+            self.keys[key_index].deleted = false; //a deleted submission overwritten should not be marked deleted
             return true;
         } else { //if key didn't exist, the queue array has grown
-            keyIndex = self.keys.length;
+            key_index = self.keys.length;
             self.keys.push();
-            self.data[key].keyIndex = keyIndex + 1;
-            self.keys[keyIndex].key = key;
+            self.data[key].key_index = key_index + 1;
+            self.keys[key_index].key = key;
             return false;
         }
     }
 
     function remove(submission storage self, uint key) internal returns (bool success) {
-        uint keyIndex = self.data[key].keyIndex;
-        if (keyIndex == 0)
+        uint key_index = self.data[key].key_index;
+        if (key_index == 0)
             return false;
         delete self.data[key];
-        self.keys[keyIndex - 1].deleted = true;
+        self.keys[key_index - 1].deleted = true;
         return true;
     }
 
     function contains(submission storage self, uint key) internal view returns (bool) {
-        return self.data[key].keyIndex > 0;
+        return self.data[key].key_index > 0;
     }
 
     function iterateStart(submission storage self) internal view returns (Iterator) {
@@ -74,16 +73,16 @@ library queueManagement {
         return iteratorSkipDeleted(self, Iterator.unwrap(iterator) + 1);
     }
         
-    function get(submission storage self, uint key) internal view returns (uint key, uint value) {
-        uint keyIndex = self.data[key].keyIndex;
-        key = self.keys[keyIndex].key;
+    function pull_next_ticket(submission storage self, uint key) internal view returns (uint key, uint value) {
+        uint key_index = self.data[key].key_index;
+        key = self.keys[key_index].key;
         value = self.data[key].value;
     }
 
-    function iteratorSkipDeleted(submission storage self, uint keyIndex) private view returns (Iterator) {
-        while (keyIndex < self.keys.length && self.keys[keyIndex].deleted)
-            keyIndex++;
-        return Iterator.wrap(keyIndex);
+    function iteratorSkipDeleted(submission storage self, uint key_index) private view returns (Iterator) {
+        while (key_index < self.keys.length && self.keys[key_index].deleted)
+            key_index++;
+        return Iterator.wrap(key_index);
     }
 }
 
@@ -92,6 +91,7 @@ contract DaDerpyDerby is ChainlinkClient, KeeperCompatibleInterface, ConfirmedOw
 
         //game data
     uint256 public daily_high_score;
+    string private score_topic = "/score";
     
         //node data
     address private oracle;
@@ -115,19 +115,36 @@ contract DaDerpyDerby is ChainlinkClient, KeeperCompatibleInterface, ConfirmedOw
 
     //note: drop queue functions here
         // need function iterating to grab deleted submissions and storing their indicies in an array
+    function join_queue() public {
+
+    }
         
 
     /**
-     * Create a Chainlink request to publish on, or subscribe to, a topic on the MQTT brokers available in the Node's External Adapter containing string data.
+     * Chainlink requests to
+            - send IPFS scripts to cl-ea-mqtt-relay for executing games (execute_sumbission)
+            - subscribe on game score topics to pull score data (grab_score)
+            - publish on game score topics for resetting score data between script executions (clear_score)
+        on the MQTT broker(s) utilized by the Node's External Adapter managing the game's state.
      */
-    function sub_int(string calldata _action, string calldata _topic, int16 _qos) public onlyOwner returns (bytes32 requestId){return pub_int(_action, _topic, _qos, 0, 1);}
-    //note: make another ipfs function, make _action parameter default for the functions, make generic call to EA
-    function pub_int(string calldata _action, string calldata _topic, int16 _qos, int256 _payload, int16 _retain) public onlyOwner returns (bytes32 requestId) 
+    function execute_submission(submission calldata _submission) private returns (bytes32 requestId){
+        string calldata _action = "ipfs";
+        return call_cl_ea_mqtt_relay(_action, _topic, _qos, 0, 1);
+    }
+    function grab_score(string calldata _action, string calldata _topic, int16 _qos) private returns (bytes32 requestId){
+        string calldata _action = "subscribe";
+        return call_cl_ea_mqtt_relay(_action, score_topic, 2, 0, 1);
+    }
+    function clear_score() private returns (bytes32 requestId){
+        string calldata _action = "publish";
+        return call_cl_ea_mqtt_relay(_action, score_topic, 2, 0, 1);
+    }
+    function call_cl_ea_mqtt_relay(string calldata _action, string calldata _topic, int16 _qos, int256 _payload, int16 _retain) private returns (bytes32 requestId)
     {
         Chainlink.Request memory request = buildChainlinkRequest(jobId_ints, address(this), this.fulfill_int.selector);
         
         // Set the params for the external adapter
-        request.add("action", _action); //options: "publish", "subscribe"
+        request.add("action", _action); //options: "publish", "subscribe", "ipfs"
         request.add("topic", _topic);
         request.addInt("qos", _qos);
         request.addInt("payload", _payload);
@@ -143,7 +160,7 @@ contract DaDerpyDerby is ChainlinkClient, KeeperCompatibleInterface, ConfirmedOw
         return data_int;
     }
 
-    function withdrawLink() public onlyOwner {
+    function withdraw_link() public onlyOwner {
         LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
         require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
     }
