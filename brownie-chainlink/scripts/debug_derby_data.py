a6c831e0a80f9432a7632186481b0e59c34a998a
Briojas <blake.a.riojas@gmail.com>
2022-05-27 07:49:37 -0500

    debug script to check upkeep

--- brownie-chainlink/scripts/debug_derby_data.py
+++ brownie-chainlink/scripts/debug_derby_data.py
@@ -7,7 +7,6 @@ def main():
     account = get_account()
     derby_contract = DaDerpyDerby[-1]
     # derby_contract.address = 0x20cD20e38c98E8D7bD8BD5bd1A82af746DCA2Ada #latest valid contract address
-
     print(derby_contract.submission_data(1))
     print(derby_contract.game())
     upkeep = derby_contract.checkUpkeep.call(b'',{"from": account})
