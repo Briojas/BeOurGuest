05b691126a1650658380e3658dfb0a54546a8f06
Briojas <blake.a.riojas@gmail.com>
2022-11-12 06:53:40 -0600

    prebuilt components from next js crash course

--- GUI/pages/index.js
+++ GUI/pages/index.js
@@ -1,5 +1,20 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 function WatchPage() {
-  return <h1>The Home Page</h1>;
+  return (
+    <Fragment>
+      <h1>The Home Page</h1>;
+      <ul>
+        <li>
+          <Link href={"/play"}>Play</Link>
+        </li>
+        <li>
+          <a href="/host">Host</a>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default WatchPage;
