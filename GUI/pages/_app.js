193c02f2d5170f08db3fcad3dd884ad7d3f40731
Briojas <blake.a.riojas@gmail.com>
2022-11-15 10:59:52 -0600

    testing Material UI components

--- GUI/pages/_app.js
+++ GUI/pages/_app.js
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import Layout from "../components/Layout";
 import { Web3ReactProvider } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
+import { Fragment } from "react";
+import Head from "next/head";
 
 const getLibrary = (provider) => {
   return new Web3Provider(provider);
@@ -9,11 +16,16 @@ const getLibrary = (provider) => {
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Web3ReactProvider>
+    <Fragment>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Web3ReactProvider>
+    </Fragment>
   );
 }
 
--- GUI/pages/engage.js
+++ GUI/pages/engage.js
@@ -1,5 +1,21 @@
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+
 function EngagePage() {
-  return <h1>The Engage Page</h1>;
+  return (
+    <div>
+      <h1>Engage with a host:</h1>
+      <TextareaAutosize
+        maxRows={4}
+        aria-label="maximum height"
+        placeholder="Maximum 4 rows"
+        defaultValue="{script:
+        {
+          action:[]
+        }}"
+        style={{ width: 600 }}
+      />
+    </div>
+  );
 }
 
 export default EngagePage;
